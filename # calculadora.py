e36819694260c8ec6b6016c1a64c3317a3eebce1
João Pinheiro <54376772+joaoipinheiro@users.noreply.github.com>
2023-03-10 10:39:30 -0300

    Add files via upload

---
+++ # calculadora.py
@@ -0,0 +1,17 @@
+# calculadora
+
+print (Calculadora)
+
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite um número: "))
+
+resultado = num1 + num2
+
+print (resultado)
+
+# tem a opção subtrair, multiplicar, dividir. basta trocar o símbolo.
+
+#Soma: +
+#Subtrair: -
+#Multiplicar: *
+#Dividir: /
